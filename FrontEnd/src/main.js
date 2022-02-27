ee4a84697c7b6efdf132905514f55e876dcda2b0
unknown <moussaassaf@gmail.com>
2022-02-27 16:18:25 +0000

    bootstrap + register page design

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Axios from 'axios'
+import 'bootstrap' 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Axios.defaults.baseURL = (process.env.API_PATH !== 'production') ? 'http://localhost:8000/api/' : '';
 
