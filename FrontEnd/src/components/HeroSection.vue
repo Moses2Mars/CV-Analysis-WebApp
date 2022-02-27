047e2e3fc9d73176255b13da24a6f705753b08ed
unknown <moussaassaf@gmail.com>
2022-02-27 21:16:05 +0000

    home sections

--- FrontEnd/src/components/HeroSection.vue
+++ FrontEnd/src/components/HeroSection.vue
@@ -13,30 +13,20 @@
         <div class="bg-circle-3 bg-circle"></div>
         <div class="bg-circle-4 bg-circle"></div>
     </header>
-    <section id="scroll">
-        <div class="container px-5">
-          <div class="row gx-5 align-items-center">
-              <div class="col-lg-6 order-lg-2">
-                  <div class="p-5"><img class="img-fluid rounded-circle" src="../assets/img/GoalCropped.jpg" alt="..." /></div>
-              </div>
-              <div class="col-lg-6 order-lg-1">
-                  <div class="p-5">
-                      <h2 class="display-4">Our Goal: </h2>
-                      <p>What we aim to do with this project is to provide candidates a quick and easy way to apply to jobs, 
-                        and a way to make the hiring process for job seekers a lot easier and less stressfull.</p>
-                  </div>
-              </div>
-          </div>
-        </div>
-    </section>
+    <GoalSection/>
+    <hr>
+    <AfterSection/>
   </div>
 </template>
 
 <script>
+import GoalSection from './GoalSection.vue'
+import AfterSection from './AfterSection.vue'
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  components: {
+    GoalSection,
+    AfterSection
   },
 };
 </script>
