99fcf6c9b2f7ef1274532ab4123903d8e6db5c04
unknown <moussaassaf@gmail.com>
2022-03-22 20:46:18 +0000

    about us card now changes color

--- FrontEnd/src/components/cardComponents/AboutUsCard.vue
+++ FrontEnd/src/components/cardComponents/AboutUsCard.vue
@@ -3,7 +3,7 @@
         <div class="card" style="width: 18rem; border-radius: 20px; box-shadow: 5px 5px #888888;">
             <div class="card-body">
                 <div class="text-start ps-4">
-                    <i :class="icon" class="blue-dash skills-icon py-4" :style="'color:'+'#'+(Math.random()*0xFFFFFF<<0).toString(16)"></i>
+                    <i :class="icon" class="blue-dash skills-icon py-4 test-class" :style="color_string"></i>
                 </div>
                 <div class="fw-bold fs-5 py-3 text-left ps-4">{{title}}</div>
                 <div class="card-text text-16 text-left ps-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
@@ -23,10 +23,26 @@ export default {
             default: '',
             type: String,
         }
+    },
+    data() {
+        return {
+            color_string: ' ',
+        }
+    },
+    methods: {
+        updateColor() {
+            this.color_string='color: #'+(Math.random()*0xFFFFFF<<0).toString(16)
+        }
+    },
+    mounted() {
+        this.updateColor();
+        setInterval(this.updateColor, 3000);
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+.test-class {
+    transition: all 0.6s ease-in-out;
+}
 </style>
